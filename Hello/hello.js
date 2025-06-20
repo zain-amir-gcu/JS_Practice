eec900e009bb9bbd721be5fef44e402b1e2aaa14
Zain Amir <zainamir.gcu@gmail.com>
2025-06-20 21:13:34 +0500

    apna college first lecture done

--- Hello/hello.js
+++ Hello/hello.js
@@ -1,1 +1,11 @@
-console.log("Welcome to Lahore");
+let age = 56;
+let name = "John";
+let isEmployed = true;
+let x;
+
+let student = {
+  name: "Jane",
+    age: 22,
+    isPass: true,
+    subjects: ["Math", "Science", "English"]
+};
